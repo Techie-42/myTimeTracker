893e4d2373c3170b8579b6baf6ec3e373ff6f58c
Techie-42 <bradley_johnston@hotmail.com>
2017-03-07 13:50:06 -0500

    Make improvements for checkstyle

--- core/src/main/java/pro/jtaylor/timetracker/core/Tracker.java
+++ core/src/main/java/pro/jtaylor/timetracker/core/Tracker.java
@@ -10,21 +10,50 @@ import java.util.List;
 public class Tracker {
     @Autowired
     private List<TimeEntry> entries;
+    
+    /**
+     * Add Method
+     * @param int add for entries
+     */
+    
 
     public void add(TimeEntry entry) {
         entries.add(entry);
     }
 
+    /**
+     * Remove Method
+     * @param int remove for entries
+     */
+ 
     public void remove(TimeEntry entry) {
         entries.remove(entry);
     }
+
+    /**
+     * Size Method
+     * @param int size for entries
+     */
+ 
 
     public int size() {
         return entries.size();
     }
 
+    /**
+     * Get Method
+     * @param TimeEntry get for entries
+     */
+ 
     public TimeEntry get(int index) {
+        
+        // boolean vaild = false;
+        // if (vaild == true) {
+        // whatever
+        //
         return entries.get(index);
     }
 }
+
+
 
